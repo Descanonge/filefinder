98455f0a002b286a9bb75130e1df5f61271a7865
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2022-08-24 16:28:49 +0200

    Do not throw if no files are found
    currently have weird in between:
    - if no files are found when exploring the filetree: throw an error
    - but if files are found, but none match, do nothing
    
    To be consistent: do not throw if no files are found

--- src/filefinder/finder.py
+++ src/filefinder/finder.py
@@ -528,10 +528,6 @@ class Finder():
 
         files.sort()
 
-        if len(files) == 0:
-            raise IndexError(f"No files were found in '{self.root}' "
-                             f"for regex '{self.regex}'.")
-
         files_matched = []
         for f in files:
             try:

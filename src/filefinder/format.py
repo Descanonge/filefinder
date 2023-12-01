1116e20cb3aad39172724541c83dd1918f2822fb
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2023-12-01 16:15:00 +0100

    Refactor Formats some more
    More efficient and clearer regex generation
    
    Added docs

--- src/filefinder/format.py
+++ src/filefinder/format.py
@@ -24,6 +24,7 @@ The regex generation and parsing are tested in `tests/unit/test_format.py`.
 # at the root of this project. © 2021 Clément Haëck
 
 import re
+from collections.abc import Mapping
 from typing import Any
 
 FORMAT_REGEX = (
@@ -44,22 +45,28 @@ FORMAT_PATTERN = re.compile(FORMAT_REGEX)
 
 
 class FormatAbstract:
-    """Parse a format string.
+    """Represent a format string.
 
-    Out of found parameters:
-    - generate regular expression
-    - format value
-    - parse string into value
+    Can generate an appropriate regular expression corresponding to that format string
+    (to some limitations), generate a string from a value, or parse such a string into
+    a value.
+
+    Users are not meant to instanciate those objects directly, use :func:`get_format`
+    instead (or its alias for retro-compatibility :func:`Format`).
 
     Parameters
     ----------
-    fmt: str
+    fmt:
         Format string.
+    params:
+        Mapping of options/parameters of the format mini-language to their values.
+        (type, fill, align, sign, alternate, zero, width, grouping, precision).
+        They should not contain None values.
     """
 
     ALLOWED_TYPES = "sdfeE"
 
-    def __init__(self, fmt: str, params: dict[str, Any]):
+    def __init__(self, fmt: str, params: Mapping[str, Any]):
         self.fmt: str = fmt
 
         self.type: str = params["type"]
@@ -80,6 +87,31 @@ class FormatAbstract:
     def format(self, value: Any) -> str:
         """Return formatted string."""
         return f"{{:{self.fmt}}}".format(value)
+
+    def get_fill_regex(self):
+        """Return regex for matching fill characters."""
+        return f"{re.escape(self.fill)}*"
+
+    def add_outer_alignement(self, rgx: str) -> str:
+        """Add necessary regex for alignement characters.
+
+        If width is not specified, does nothing.
+        """
+        if self.width == 0 or self.align == "=":
+            return rgx
+
+        out = [rgx]
+        fill_rgx = self.get_fill_regex()
+
+        # value is right-aligned or center
+        if self.align in ">^":
+            out.insert(0, fill_rgx)
+
+        # value is left-aligned or center
+        if self.align in "<^":
+            out.append(fill_rgx)
+
+        return "".join(out)
 
     def parse(self, s: str) -> Any:
         """Parse string generated with this format into an appropriate value."""
@@ -104,46 +136,19 @@ class FormatString(FormatAbstract):
         """
         return s
 
-    def insert_alignement(self, rgx: str) -> str:
-        """Add necessary regex for alignement characters.
-
-        If width is not specified, does nothing.
-        """
-        if self.width == 0:
-            return rgx
-
-        if self.align == '=':
-            raise ValueError("'=' align not supported for 's' format.")
-
-        # Fill: the fill character any number of time, greedy
-        fill_rgx = f"{re.escape(self.fill)}*"
-
-        out_rgx = ""
-
-        # value is right-aligned or center
-        if self.align in ">^":
-            out_rgx += fill_rgx
-
-        out_rgx += rgx
-
-        # value is left-aligned or center
-        if self.align in "<^":
-            out_rgx += fill_rgx
-
-        return out_rgx
-
-
     def generate_expression(self) -> str:
         """Generate a regular expression matching strings created with this format.
 
         Will match any character, non-greedily.
         """
         rgx = ".*?"
-        rgx = self.insert_alignement(rgx)
+        rgx = self.add_outer_alignement(rgx)
         return rgx
 
 
-class FormatNumber(FormatAbstract):
+class FormatNumberAbstract(FormatAbstract):
+    """Represent a format string for numbers (type d, f, e, E)."""
+
     ALLOWED_TYPES = "dfeE"
 
     def remove_special(self, s: str) -> str:
@@ -161,15 +166,18 @@ class FormatNumber(FormatAbstract):
         return re.sub(pattern, "", s)
 
     def get_left_of_decimal(self) -> str:
-        """Get regex for numbers left of decimal point."""
+        """Get regex for the numbers left of decimal point.
+
+        Will deal with grouping if present.
+        """
         if self.grouping:
             rgx = rf"\d?\d?\d(?:{self.grouping}\d{{3}})*"
         else:
             rgx = r"\d+"
         return rgx
 
-    def get_sign(self) -> str:
-        """Get sign regex."""
+    def get_sign_regex(self) -> str:
+        """Get sign regex with approprite zero padding."""
         if self.sign == "-":
             rgx = "-?"
         elif self.sign == "+":
@@ -178,48 +186,49 @@ class FormatNumber(FormatAbstract):
             rgx = r"[\s-]"
         else:
             raise KeyError("Sign not in {+- }")
+
+        # padding is added between sign and numbers
+        if self.width > 0 and self.align == "=":
+            rgx = rgx + self.get_fill_regex()
         return rgx
 
-    def insert_alignement(self, rgx: str) -> str:
-        fill_rgx = ""
-        if self.width > 0:
-            fill_rgx += f"{re.escape(self.fill)}*"
-        out_rgx = ""
 
-        if self.align in ">^":
-            out_rgx += fill_rgx
+class FormatInteger(FormatNumberAbstract):
+    """Represent a format string for integers (type d)."""
 
-        out_rgx += self.get_sign()
-
-        if self.align == "=":
-            out_rgx += fill_rgx
-
-        out_rgx += rgx
-
-        if self.align in "<^":
-            out_rgx += fill_rgx
-
-        return out_rgx
-
-
-class FormatInteger(FormatNumber):
     ALLOWED_TYPES = "d"
 
     def parse(self, s: str) -> int:
+        """Parse string generated with format.
+
+        This simply use int() to parse strings. Those are thrown
+        off when using fill characters (other than 0), or thousands groupings,
+        so we remove these from the string.
+
+        Parsing will fail for some deviously chaotic formats such as using the '-' fill
+        character on a negative number, or when padding with numbers.
+        """
         s = self.remove_special(s)
         return int(s)
 
     def generate_expression(self) -> str:
         """Generate regex from format string."""
-        rgx = self.get_left_of_decimal()
-        rgx = self.insert_alignement(rgx)
+        rgx = self.get_sign_regex() + self.get_left_of_decimal()
+        rgx = self.add_outer_alignement(rgx)
         return rgx
 
 
-class FormatFloat(FormatNumber):
+class FormatFloat(FormatNumberAbstract):
+    """Represent a format string for floats (type f, e, E)."""
+
     ALLOWED_TYPES = "feE"
 
     def get_right_of_decimal(self) -> str:
+        """Return regex for numbers after decimal points.
+
+        Including the decimal point itself. It will respect 'alternate' option and
+        the specified precision.
+        """
         rgx = ""
         if self.precision != 0 or self.alternate:
             rgx += r"\."
@@ -230,30 +239,36 @@ class FormatFloat(FormatNumber):
     def parse(self, s: str) -> float:
         """Parse string generated with format.
 
-        This simply use int() and float() to parse strings. Those are thrown
+        This simply use float() to parse strings. Those are thrown
         off when using fill characters (other than 0), or thousands groupings,
         so we remove these from the string.
 
-        Parsing will fail when using the '-' fill character on a negative
-        number, or when padding with numbers. If you use such formats, please
-        contact me to explain me why in the hell you do.
+        Parsing will fail for some deviously chaotic formats such as using the '-' fill
+        character on a negative number, or when padding with numbers.
         """
         # Remove special characters (fill or groupings)
         s = self.remove_special(s)
         return float(s)
 
     def generate_expression(self) -> str:
+        """Generate a regular expression matching strings created with this format."""
         if self.type == "f":
-            rgx = self.get_left_of_decimal()
-            rgx += self.get_right_of_decimal()
-            rgx = self.insert_alignement(rgx)
+            rgx = (
+                self.get_sign_regex()
+                + self.get_left_of_decimal()
+                + self.get_right_of_decimal()
+            )
+            rgx = self.add_outer_alignement(rgx)
             return rgx
 
         assert self.type in "eE"
-        rgx = r"\d"
-        rgx += self.get_right_of_decimal()
-        rgx += rf"{self.type}[+-]\d+"
-        rgx = self.insert_alignement(rgx)
+        rgx = (
+            self.get_sign_regex()
+            + r"\d"
+            + self.get_right_of_decimal()
+            + rf"{self.type}[+-]\d+"
+        )
+        rgx = self.add_outer_alignement(rgx)
         return rgx
 
 
@@ -284,7 +299,7 @@ def get_format(format: str) -> FormatAbstract:
 
     # defaults values for unset remaining parameters
     defaults = dict(
-        align=">", fill=" ", sign="-", width="0", precision=".6", grouping=''
+        align=">", fill=" ", sign="-", width="0", precision=".6", grouping=""
     )
     for k, v in defaults.items():
         if params[k] is None:
@@ -301,6 +316,7 @@ def get_format(format: str) -> FormatAbstract:
             f"expected one of '{list(FORMAT_CLASSES.keys())}'."
         )
     return FORMAT_CLASSES[type](format, params)
+
 
 # Retrocompatible alias
 Format = get_format

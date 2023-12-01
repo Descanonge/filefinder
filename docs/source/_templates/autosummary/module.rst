e0a81f8bb3d3243f5d3b18aff7f0079a5b8000ee
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2023-12-01 17:21:56 +0100

    Up doc config

--- docs/source/_templates/autosummary/module.rst
+++ docs/source/_templates/autosummary/module.rst
@@ -3,8 +3,8 @@
 
 .. automodule:: {{ fullname }}
    :members:
-   :special-members: __len__, __getitme__, __iter__
-   :member-order: groupwise
+   :private-members:
+   :member-order: bysource
 
    {% block attributes %}
    {% if attributes %}
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,45 +1,34 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-# import os
-# import sys
 import filefinder
 
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "filefinder"
-copyright = "2021, Clément Haëck"
-author = "Clément Haëck"
+project = 'Filefinder'
+copyright = '2021, Clément Haëck'
+author = 'Clément Haëck'
 
-# The full version, including alpha/beta/rc tags
 release = filefinder.__version__
 version = filefinder.__version__
 print(f"filefinder: {version}")
 
-master_doc = "index"
-templates_path = ["_templates"]
-exclude_patterns = ["_build"]
-
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosectionlabel",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.intersphinx",
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
+
+templates_path = ['_templates']
+# exclude_patterns = []
+
 
 # Napoleon config
 napoleon_google_docstring = False
@@ -48,67 +37,72 @@ napoleon_use_param = True
 napoleon_use_rtype = False
 napoleon_preprocess_type = False
 
+pygments_style = 'default'
+
+# Document rtype in the 'returns' directive/section
+# (get rtype from typehint)
+
 add_module_names = False
 
 # Autosummary config
 autosummary_generate = ["api.rst"]
 
 # Autodoc config
-autodoc_typehints = "description"
-autodoc_typehints_format = "short"
+autodoc_typehints = 'description'
+autodoc_typehints_format = 'short'
+autodoc_type_aliases = {
+    # show the full path
+    'xr.DataArray': 'xarray.DataArray',
+    'xr.Dataset': 'xarray.Dataset',
+    # do not show the full path
+    'collections.abc.Sequence': '~collections.abc.Sequence'
+}
+napoleon_type_aliases = autodoc_type_aliases.copy()
 
 # Intersphinx config
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/3/", None),
-    "numpy": ("https://numpy.org/doc/stable", None),
-    "dask": ("https://docs.dask.org/en/latest", None),
-    "xarray": ("https://docs.xarray.dev/en/stable/", None),
+    'python': ('https://docs.python.org/3/', None),
+    'xarray': ('https://docs.xarray.dev/en/stable/', None)
 }
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_book_theme"
-html_static_path = ["_static"]
-html_css_files = ["css/custom.css"]
-html_title = "heterogeneity-index"
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+html_title = 'heterogeneity-index'
 html_theme_options = dict(
     collapse_navigation=False,
     use_download_button=True,
     use_fullscreen_button=False,
+
     # TOC
     show_toc_level=2,
+
     # Link to source in repo
-    repository_url="https://github.com/Descanonge/filefinder",
+    repository_url='https://github.com/Descanonge/filefinder',
     use_source_button=True,
-    repository_branch="master",
-    path_to_docs="doc",
+    repository_branch='master',
+    path_to_docs='doc',
+
     # Social icons
     icon_links=[
-        dict(
-            name="Repository",
-            url="https://github.com/Descanonge/filefinder",
-            icon="fa-brands fa-square-github",
-        ),
-        dict(
-            name="Documentation",
-            url="https://filefinder.readthedocs.io",
-            icon="fa-solid fa-book",
-        ),
+        dict(name='Repository',
+             url='https://github.com/Descanonge/filefinder',
+             icon='fa-brands fa-square-gitlab'),
+        dict(name='Documentation',
+             url='https://filefinder.readthedocs.io',
+             icon='fa-solid fa-book')
     ],
+
     # Footer
-    article_footer_items=["prev-next"],
-    content_footer_items=[],
-    footer_start=["footer-left"],
-    footer_end=["footer-right"],
+    article_footer_items = ['prev-next'],
+    content_footer_items = [],
+    footer_start = ['footer-left'],
+    footer_end = ['footer-right'],
 )
-html_last_updated_fmt = "%Y-%m-%d"
+html_last_updated_fmt = '%Y-%m-%d'
 
-html_sidebars = {"**": ["navbar-logo.html", "sbt-sidebar-nav.html", "icon-links.html"]}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_sidebars = {
+    '**': ['navbar-logo.html', 'sbt-sidebar-nav.html', 'icon-links.html']
+}

cb05831f330c28a49030864b2631423a29043fc9
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2024-04-01 02:24:33 +0200

    Use hypothesis to make tests
    Useful to test more thoroughly different numbers input

--- tests/unit/test_format.py
+++ tests/unit/test_format.py
@@ -18,13 +18,18 @@ failure than the actual parsing code...).
 import re
 
 import pytest
-from filefinder.format import Format
+from filefinder.format import (
+    DangerousFormatError,
+    Format,
+    FormatAbstract,
+    FormatFloat,
+    FormatInteger,
+)
+from hypothesis import given
+from hypothesis import strategies as st
 
-numbers_d = [0, 1, -1, -10, 100, 1000, 12509123, -12093124]
-numbers_f = [1.123, -3451.1209, 0.012, 1e-8]
 
-
-def assert_format(string, fmt):
+def assert_format(string: str, fmt: FormatAbstract):
     pattern = fmt.generate_expression()
     m = re.fullmatch(pattern, string)
     assert (
@@ -32,19 +37,19 @@ def assert_format(string, fmt):
     ), f"No match. Format '{fmt.fmt}'. Pattern '{pattern}'. String '{string}'"
 
 
-def assert_parse_i(number, string, fmt):
+def assert_parse_int(number: int, string: str, fmt: FormatInteger):
     parsed = fmt.parse(string)
     assert (
         number == parsed
     ), f"Not parsed. Format '{fmt.fmt}'. Number '{number}'. Parsed '{parsed}'"
 
 
-def assert_parse_f(number, string, fmt, precision):
+def assert_parse_float(number: float, string: str, fmt: FormatFloat, precision: str):
     if precision == "":
-        precision = 6
+        decimals = 6
     else:
-        precision = int(precision[-1])
-    number = round(number, precision)
+        decimals = int(precision[-1])
+    number = round(number, decimals)
 
     parsed = fmt.parse(string)
     assert (
@@ -52,55 +57,93 @@ def assert_parse_f(number, string, fmt, precision):
     ), f"Not parsed. Format '{fmt.fmt}'. Number '{number}'. Parsed '{parsed}'"
 
 
-signs = ["", "+", "-", " "]
-zeros = ["", "0"]
-alts = ["", "#"]
-aligns = ["", "<", ">", "=", "^", "a>"]
-groupings = ["", ",", "_"]
-widths = ["", "0", "3", "8"]
-precisions = ["", ".0", ".1", ".3", ".6"]
+fill = st.text(
+    alphabet=st.characters(
+        codec="utf-8", exclude_characters=["{", "}"], categories=["L", "P", "S"]
+    ),
+    min_size=0,
+    max_size=1,
+)
+align = ["", "<", ">", "=", "^"]
+sign = ["", "+", "-", " "]
+alt = ["", "#"]
+zero = ["", "0"]
+grouping = ["", ",", "_"]
+width = st.one_of(st.just(""), st.integers(min_value=0, max_value=256).map(str))
+precision = st.one_of(
+    st.just(""), st.integers(min_value=0, max_value=64).map(lambda i: f".{i}")
+)
 
 
-@pytest.mark.parametrize("align", aligns)
-@pytest.mark.parametrize("sign", signs)
-@pytest.mark.parametrize("width", widths)
-@pytest.mark.parametrize("grouping", groupings)
-@pytest.mark.parametrize("number", numbers_d)
-def test_format_d(align, sign, width, grouping, number):
-    fmt = Format(align + sign + width + grouping + "d")
-    s = fmt.format(number)
-    assert_format(s, fmt)
-    assert_parse_i(number, s, fmt)
+@pytest.mark.parametrize("align", align)
+@pytest.mark.parametrize("sign", sign)
+@pytest.mark.parametrize("alt", alt)
+@pytest.mark.parametrize("zero", zero)
+@pytest.mark.parametrize("grouping", grouping)
+@given(fill=fill, width=width, number=st.integers())
+def test_format_d(fill, align, sign, alt, zero, width, grouping, number):
+    format_string = fill + align if align else ""
+    format_string += sign + alt + zero + width + grouping + "d"
 
-
-@pytest.mark.parametrize("align", aligns)
-@pytest.mark.parametrize("sign", signs)
-@pytest.mark.parametrize("width", widths)
-@pytest.mark.parametrize("grouping", groupings)
-@pytest.mark.parametrize("precision", precisions)
-@pytest.mark.parametrize("number", numbers_d + numbers_f)  # type: ignore
-def test_format_f(align, sign, width, grouping, precision, number):
-    fmt = Format(align + sign + width + grouping + precision + "f")
-    s = fmt.format(number)
-    assert_format(s, fmt)
-    assert_parse_f(number, s, fmt, precision)
-
-
-@pytest.mark.parametrize("align", aligns)
-@pytest.mark.parametrize("sign", signs)
-@pytest.mark.parametrize("width", widths)
-@pytest.mark.parametrize("precision", precisions)
-@pytest.mark.parametrize("number", numbers_d + numbers_f)  # type: ignore
-def test_format_e(align, sign, width, precision, number):
-    fmt = Format(align + sign + width + precision + "e")
+    try:
+        fmt = Format(format_string)
+    except DangerousFormatError:
+        return
     s = fmt.format(number)
     assert_format(s, fmt)
 
+    assert_parse_int(number, s, fmt)
 
-@pytest.mark.parametrize("align", ["", "<", "%<", ">", " >", "x^"])
-@pytest.mark.parametrize("width", ["", "0", "3", "12"])
-@pytest.mark.parametrize("value", ["a", "Dorothy", "zx3000", "w[ponpon]"])
-def test_format_s(align, width, value):
-    fmt = Format(align + width + "s")
-    s = fmt.format(value)
+
+@pytest.mark.parametrize("align", align)
+@pytest.mark.parametrize("sign", sign)
+@pytest.mark.parametrize("alt", alt)
+@pytest.mark.parametrize("zero", zero)
+@pytest.mark.parametrize("grouping", grouping)
+@given(
+    fill=fill,
+    width=width,
+    precision=precision,
+    number=st.integers(),
+)
+def test_format_f(fill, align, sign, alt, zero, grouping, width, precision, number):
+    format_string = fill + align if align else ""
+    format_string += sign + alt + zero + width + grouping + precision + "f"
+
+    try:
+        fmt = Format(format_string)
+    except DangerousFormatError:
+        return
+    s = fmt.format(number)
     assert_format(s, fmt)
+    assert_parse_float(number, s, fmt, precision)
+
+
+@pytest.mark.parametrize("align", align)
+@pytest.mark.parametrize("sign", sign)
+@pytest.mark.parametrize("alt", alt)
+@pytest.mark.parametrize("zero", zero)
+@pytest.mark.parametrize("grouping", grouping)
+@given(
+    fill=fill,
+    width=width,
+    precision=precision,
+    number=st.floats(),
+)
+def test_format_e(fill, align, sign, alt, zero, width, grouping, precision, number):
+    format_string = fill + align if align else ""
+    format_string += sign + alt + zero + width + grouping + precision + "e"
+
+    try:
+        fmt = Format(format_string)
+    except DangerousFormatError:
+        return
+    s = fmt.format(number)
+    assert_format(s, fmt)
+    assert_parse_float(number, s, fmt, precision)
+
+
+# def test_format_s(align, width, value):
+#     fmt = Format(align + width + "s")
+#     s = fmt.format(value)
+#     assert_format(s, fmt)

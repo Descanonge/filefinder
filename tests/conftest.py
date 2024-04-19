be34e2d4c8456f95acd26f89a153a4fedf24622f
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2024-04-19 12:32:46 +0200

    Do not raise on slow generation for CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 import os
 
-from hypothesis import Verbosity, settings
+from hypothesis import HealthCheck, Verbosity, settings
 
-settings.register_profile("ci", max_examples=1000)
+settings.register_profile(
+    "ci", max_examples=1000, suppress_health_check=[HealthCheck.too_slow]
+)
 settings.register_profile("dev", max_examples=50)
 settings.register_profile("debug", max_examples=50, verbosity=Verbosity.verbose)
 
